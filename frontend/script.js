270b01203e497522fd4cce773790463aa5246ea5
Alien Coder <paidcodecamp@gmail.com>
2023-08-06 16:39:25 +0530

    test: deploy

--- frontend/script.js
+++ frontend/script.js
@@ -2,13 +2,11 @@ const submitButton = document.getElementById("submitButton");
 const messageInput = document.getElementById("messageInput");
 const messageOutput = document.getElementById("messageOutput");
 
-const baseUrl = "http://localhost:8080/api";
-
 submitButton.addEventListener("click", () => {
   const message = messageInput.value;
   console.log("Sending message:", message);
   if (message.trim() !== "") {
-    fetch(`${baseUrl}/corrections`, {
+    fetch(`api/corrections`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
