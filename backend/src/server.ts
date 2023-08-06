e1de4acacbe96864a5a4e1334f135e54f1d4888c
Alien Coder <paidcodecamp@gmail.com>
2023-08-06 15:43:20 +0530

    feat: serve html from root URL

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { Request, Response } from "express";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
+
 import correctionsRouter from "./routes/corrections";
 import db from "./database";
 
@@ -16,6 +18,15 @@ app.use(
 app.use(express.json());
 
 app.use("/api", correctionsRouter);
+
+// Define the path to your HTML file
+const staticFilesPath = path.join(__dirname, "../../frontend");
+app.use(express.static(staticFilesPath));
+
+// Serve the HTML file when a GET request is made to the root URL
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(staticFilesPath);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
