5f951047e8956b6679791faf4c8667f4c79930e1
Alien Coder <paidcodecamp@gmail.com>
2023-08-06 15:30:02 +0530

    added: correctionsRouter to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import correctionsRouter from "./routes/corrections";
 import db from "./database";
 
 dotenv.config();
@@ -14,20 +15,7 @@ app.use(
 );
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  const query = "SELECT * FROM corrections ORDER BY RANDOM() LIMIT 1";
-  db.get(query, (err: Error | null, row) => {
-    if (err) {
-      console.error(
-        "Error fetching random correction from the database:",
-        err.message
-      );
-      res.status(500).json({ error: "Error fetching random correction" });
-    } else {
-      res.json(row);
-    }
-  });
-});
+app.use("/api", correctionsRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
